eb8f9b0aad7c3a773a7b22f55c6dfc8e5b2d6a75
elmir <elmirbek2000kg>
2025-05-23 02:47:56 +0300

    saved

--- src/pages/InnerPageCards.jsx
+++ src/pages/InnerPageCards.jsx
@@ -7,38 +7,12 @@ import "swiper/css/navigation";
 import "swiper/css/thumbs";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Thumbs } from "swiper/modules";
-<<<<<<< HEAD
-import BreadCrumbs from "../components/UI/BreadCrumbs";
-import { useSelector } from "react-redux";
-
-const colorTranslations = {
-  black: "Чёрный",
-  white: "Белый",
-  red: "Красный",
-  blue: "Синий",
-  green: "Зелёный",
-  yellow: "Жёлтый",
-  beige: "Бежевый",
-  gray: "Серый",
-};
-=======
 import { useDispatch } from "react-redux";
 import { addToBasket } from "../store/reducer/cardMainSlice";
->>>>>>> f7d40b6e52168b498c0b44cfaddc6854049c88d4
 
 export const InnerPageCards = () => {
   const { womanCardAdmin } = useSelector((state) => state.cardsSlicer);
   const { cardId } = useParams();
-<<<<<<< HEAD
-
-  const selectedCard = womanCardAdmin.find(
-    (card) => card.id === Number(cardId)
-  );
-
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-
-=======
   const dispatch = useDispatch();
   const selectedCard = cards.find((card) => card.id === Number(cardId));
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -46,30 +20,9 @@ export const InnerPageCards = () => {
   const handleAddToBasket = () => {
     dispatch(addToBasket(selectedCard));
   };
->>>>>>> f7d40b6e52168b498c0b44cfaddc6854049c88d4
   if (!selectedCard) return <p>Карточка не найдена</p>;
 
   return (
-<<<<<<< HEAD
-    <>
-      <StyledBreadCrumbsContainer>
-        <BreadCrumbs />
-      </StyledBreadCrumbsContainer>
-      <Wrapper>
-        <StyledImageBlock>
-          <SmallImageBlock>
-            <Swiper
-              direction="vertical"
-              onSwiper={setThumbsSwiper}
-              slidesPerView={4}
-              spaceBetween={-30}
-              navigation
-              watchSlidesProgress
-              modules={[Thumbs, Navigation]}
-              onSlideChange={(swiper) =>
-                setActiveSlideIndex(swiper.activeIndex)
-              }
-=======
     <Wrapper>
       <StyledImageBlock>
         <SmallImageBlock>
@@ -121,91 +74,17 @@ export const InnerPageCards = () => {
               key={index}
               onClick={() => setSelectedColorIndex(index)}
               $isSelected={index === selectedColorIndex}
->>>>>>> f7d40b6e52168b498c0b44cfaddc6854049c88d4
             >
-              {selectedCard.colors?.map((img, index) => (
-                <SwiperSlide key={index}>
-                  <StyledImage
-                    src={img.image}
-                    alt="thumb"
-                    style={{ height: "87px" }}
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </SmallImageBlock>
-          <StyledMainImage>
-            <Swiper
-              navigation
-              thumbs={{ swiper: thumbsSwiper }}
-              modules={[Navigation, Thumbs]}
-              style={{ width: "100%", height: "100%" }}
-              onSlideChange={(swiper) =>
-                setActiveSlideIndex(swiper.activeIndex)
-              }
-            >
-              {selectedCard.images?.map((img, index) => (
-                <SwiperSlide key={index}>
-                  <img
-                    src={img}
-                    alt="main"
-                    onClick={() => setActiveSlideIndex(index)}
-                    style={{ cursor: "pointer" }}
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </StyledMainImage>
-        </StyledImageBlock>
-        <WrapperDataProduct>
-          <BoxTitlePrice>
-            <p>{selectedCard.name}</p>
-            <span>KGS{selectedCard.price}</span>
-          </BoxTitlePrice>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-              alignItems: "center",
-            }}
-          >
-            <div style={{ display: "flex", gap: "50px" }}>
-              {selectedCard.colors?.[activeSlideIndex] && (
-                <StyledSquaresdiv
-                  color={selectedCard.colors[activeSlideIndex].colorsquare}
-                  isSelected={true}
-                  style={{ cursor: "pointer" }}
-                />
-              )}
-            </div>
-
-            <div>
-              {selectedCard.colors?.[activeSlideIndex] && (
-                <span>
-                  {colorTranslations[
-                    selectedCard.colors[activeSlideIndex].colorsquare
-                      .toLowerCase()
-                      .trim()
-                  ] || selectedCard.colors[activeSlideIndex].colorsquare}
-                </span>
-              )}
-            </div>
+              <img src={clr.color} alt={clr.colorTitle} />
+              {index === selectedColorIndex && <span>{clr.colorTitle}</span>}
+            </BoxColor>
+          ))}
+        </div>
+        <BoxSizes>
+          <p>Таблица размеров</p>
+          <div>
+            <div>{selectedCard.size}</div>
           </div>
-<<<<<<< HEAD
-
-          <BoxSizes>
-            <p>Таблица размеров</p>
-            <div>
-              <div>{selectedCard.size}</div>
-            </div>
-            <p>Товар будет доставлен в течении 10 дней</p>
-          </BoxSizes>
-          <BaseButton sx={{ marginTop: "82%" }}>Добавить в корзину</BaseButton>
-        </WrapperDataProduct>
-      </Wrapper>
-    </>
-=======
           <p>Товар будет доставлен в течении 10 дней</p>
         </BoxSizes>
         <BaseButton sx={{ marginTop: "82%" }} onClick={handleAddToBasket}>
@@ -213,7 +92,6 @@ export const InnerPageCards = () => {
         </BaseButton>
       </WrapperDataProduct>
     </Wrapper>
->>>>>>> f7d40b6e52168b498c0b44cfaddc6854049c88d4
   );
 };
 
@@ -222,12 +100,9 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: flex-start;
   gap: "71px";
-<<<<<<< HEAD
   margin-top: 130px;
-=======
   margin-top: 150px;
   margin-bottom: 100px;
->>>>>>> f7d40b6e52168b498c0b44cfaddc6854049c88d4
 `;
 const StyledImageBlock = styled.div`
   display: flex;
